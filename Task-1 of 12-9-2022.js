17180713a05cfd9311441380c451f9325eb529a7
M0410 <108316894+M0410@users.noreply.github.com>
2022-09-14 18:40:54 +0530

    12-9-2022
    12-9-2022

---
+++ Task-1 of 12-9-2022.js
@@ -0,0 +1,25 @@
+let day="Monday";
+if(day=="Monday"){
+    console.log("Today is Monday.");
+}
+else if(day=="Tuesday"){
+    console.log("Today is Tuesday");
+}
+else if(day=="Wednresday"){
+    console.log("Today is Wednesday");
+}
+else if(day=="Thursday"){
+    console.log("Today is Thursday");
+}
+else if(day=="Friday"){
+    console.log("Today is Friday");
+}
+else if(day=="Saturday"){
+    console.log("Today is Saturday");
+}
+else if(day=="Sunday"){
+    console.log("Today is Sunday");
+}
+else{
+    console.log("choise");
+}
